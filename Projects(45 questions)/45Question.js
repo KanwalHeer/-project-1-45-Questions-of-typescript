36302f43dcb903f0ecec2ab517752636dc277abc
KanwalHeer <160328670+KanwalHeer@users.noreply.github.com>
2024-02-17 06:07:14 -0800

    Add files via upload

---
+++ Projects(45 questions)/45Question.js
@@ -0,0 +1,421 @@
+// //Question # 02:
+// let myName:string = "Kanwal";
+// console.log("Hello Kanwal, whould you like to learn some typescript today" + myName);
+// //Question # 03:
+// let yourName:string = "fiza"
+// console.log(yourName.toUpperCase());
+// console.log(yourName.toLowerCase());
+//      // console.log(yourName.toTitlecase()) ==> sorry i cand find title case in ttypescript,
+// // Question # 04:
+// let autherName:string = "Walt Disney"
+// console.log(`${autherName} Once said,"It is fun to do the impossible."`);
+// //Question # 05:
+// let nweAuther:string = "Walt Disney";
+// let message:string = '  Once said,"It is fun to do the impossible."';
+// console.log(nweAuther  +  message);
+// //Question # 06:
+// let personName0:string = "\tAli\t";
+// console.log(personName0);
+// let personName1:string = "Ali";
+// console.log(personName1);
+// let personName2:string = " Hi\nAli\n";
+// console.log(personName2);
+// let personName3:string = "Ali";
+// console.log(personName3);
+// //Question # 07:
+// console.log(4+4,16-8,2*4,24/3);
+// //Question # 08:
+// console.log(5 +3);
+// console.log(9-1);
+// console.log(2*4);
+// console.log(16/2);
+// //Question # 09:
+// let favoriteNumber:number = 4;
+// console.log(`${favoriteNumber} is my favorite Number and it is also lucky for me.`);
+// // Question # 10:
+// let comment:string = "this question is for 'comments'";
+// //The above sentence is a variable and its type is string;
+// console.log(comment);
+// //Question # 11:
+// let names:string[]=["Fiza","Rabia","Ureba","Ammara"];
+// console.log(names[0]);
+// console.log(names[1]);
+// console.log(names[2]);
+// console.log(names[3]);
+// //Question # 12:
+// let names1:string[]=["Fiza","Rabia","Ureba","Ammara"];
+// console.log( `Good Morning ${names[0]}`);
+// console.log(`Good Morning ${names[1]}`);
+// console.log(`Good Morning ${names[2]}`);
+// console.log(`Good Morning ${names[3]}`);
+// //Question # 13:
+// let transportation:string[]=["car","bike","riksha","aroplane"];
+// console.log( `I want to buy my own  ${transportation[0]}.`);
+// console.log(`I really want to ride a  ${transportation[1]}.`);
+// console.log(`i like ${transportation[2]} ride.`);
+// console.log(`${transportation[3]} is my favorite one transportatrion.`);
+//Question # 14:
+// let guestList:string[] =["Komal","Aqsa","Areeba","Urooj"] ;
+// console.log(` Dear ${guestList[0]} I would like to invite you at dinner.`);
+// console.log(` Dear ${guestList[1]} I would like to invite you at dinner.`);
+// console.log(` Dear ${guestList[2]} I would like to invite you at dinner.`);
+// console.log(` Dear ${guestList[3]} I would like to invite you at dinner.`);
+// //Question # 15:
+// let guestList1:string[] =["Komal","Aqsa","Areeba","Urooj"] ;
+// console.log(` Dear ${guestList1[0]} I would like to invite you at dinner.`);
+// console.log(` Dear ${guestList1[1]} I would like to invite you at dinner.`);
+// console.log(` Dear ${guestList1[2]} I would like to invite you at dinner.`);
+// console.log(` Dear ${guestList1[3]} I would like to invite you at dinner.`);
+// console.log(`${guestList1[1]} will not come at my dineer`);
+// guestList1.splice(1,1,"Mahnoor");
+// console.log(` Dear ${guestList1[0]} I would like to invite you at dinner.`);
+// console.log(` Dear ${guestList1[1]} I would like to invite you at dinner.`);
+// console.log(` Dear ${guestList1[2]} I would like to invite you at dinner.`);
+// console.log(` Dear ${guestList1[3]} I would like to invite you at dinner.`);
+// //Question # 16:
+// let guestList2:string[] =["Komal","Aqsa",] ;
+// console.log(` Dear ${guestList2[0]} I would like to invite you at dinner.`);
+// console.log(` Dear ${guestList2[1]} I would like to invite you at dinner.`);
+// console.log("I am going to make a big dinner to all you")
+// guestList2.unshift("Heer");
+// guestList2.splice(2,0,"Maheen")
+// guestList2.push("Abiha")
+// console.log(` Dear ${guestList2[0]} I would like to invite you at dinner.`);
+// console.log(` Dear ${guestList2[1]} I would like to invite you at dinner.`);
+// console.log(` Dear ${guestList2[2]} I would like to invite you at dinner.`);
+// console.log(` Dear ${guestList2[3]} I would like to invite you at dinner.`);
+// console.log(` Dear ${guestList2[4]} I would like to invite you at dinner.`);
+// //Question # 17:
+// console.log("Sorry dear guests i can only invite two persons.");
+// console.log(` sorry dear ${guestList2.pop()} i can not invite you`);
+// console.log(` sorry dear ${guestList2.pop()} i can not invite you`);
+// console.log(` sorry dear ${guestList2.pop()} i can not invite you`);
+// console.log(` Dear ${guestList2[0]} you are still invited.`);
+// console.log(` Dear ${guestList2[1]} you are still invited.`);
+// guestList2.pop();
+// guestList2.pop();
+// console.log(guestList2);
+// //Question # 18:
+// // let locations:string[] = ["Pakistan","Banghladash","Indonasia","Dubai"];
+// // console.log(locations);
+// // console.log(locations.sort());
+// // console.log(locations[3],locations[0],locations[2],locations[1]);
+// // console.log(locations.reverse());
+// // console.log(locations[0],locations[3],locations[1],locations[2]);
+// // console.log(locations.reverse());
+// // console.log(locations.reverse())
+// // console.log(locations.sort());
+// //Question # 19:
+// console.log(`I am inviting 2 people`);
+// // Question # 20:
+// let lanuages:string[]= ["Arabic" ,"Urdu" ,"English", "Hindi","Balochi" ,"Sindhi","Punjabi"]
+// console.log(lanuages);
+// // Question # 21:
+//  let myObject ={
+//     country:"Pakistan",
+//     city:"Karachi",
+//     moutain: "k2",
+//     lanuage: "Urdu",
+//  }
+//  console.log(myObject);
+//  //Question # 22:
+//  let indexs:number[] = [1,2,3];
+//  console.log(indexs[3]);
+//Question # 23:
+//  let num1:number = 4;
+//  let num2:number = 6;
+//  let name1:string = "Heer";
+//  let name2:string = "Kanawal";
+//  let car:string = "honda";
+//  let country:string = "Pakistan";
+//  console.log(num1 ==num2);
+//  console.log(num1 > num2);
+//  console.log(car !== "honda");
+//  console.log(name1 == name2);
+//  console.log(car == "Pakistan");
+//  console.log(car == "honda");
+//  console.log(country=="Pakistan");
+//  console.log(num1 !==num2);
+//  console.log(num1 < num2);
+//  console.log(country!=="Pakistani");
+//  //Question # 24:
+//  console.log("hello"=="hello")
+//  console.log("hello"!=="hello")
+//  let car1:string = "Honda"
+//  let car2:string = "honda"
+//  console.log(car1 == car2)
+//  console.log(car1 !== car2)
+//  let number1:number = 5
+//  let number2:number = 6
+//  console.log(number1 == number2)
+//  console.log(number1 !== number2)
+//  console.log(car1 == car2 || number1!==number2)
+//  console.log(car1 == car2 || number1==number2)
+//  console.log(car1 == car2 && number1 ==number2)
+//  console.log(car1 == car2 && number1!==number2)
+//  let array:(number | string)[]=[1,4,5,3,6,7]
+// function isArray(Value:number | string): boolean {
+//     return array.includes(Value)
+// }
+// console.log(isArray(2))
+// console.log(isArray(7))
+// console.log(isArray(9))
+// console.log(isArray(4))
+// console.log(isArray(4))
+// //Question # 25:
+// let Alien_color:string  = "yellow"
+// if (Alien_color == "green") {
+//     console.log("player just earned 5 points")
+// }
+// if (Alien_color !== "green") {
+// }
+// //Question # 26:
+// let color:string = "green"
+// if (Alien_color == color ) {
+//     console.log("the player just earned 5 points for shooting the alien.")
+// } else {
+//     console.log("player just earned 10 points.")
+// }
+// //Question # 27:
+// if (Alien_color == "green") {
+//     console.log("player just earned 5 points.")
+// }else if(Alien_color == "yellow") {
+//     console.log("player just earned 10 points.")
+// }else if(Alien_color == "red"){
+//     console.log("player just earned 15 points.")
+// }
+//Question # 28:
+// let personAge:number = 23
+//  if (personAge < 2) {
+//     console.log("you are a baby")
+//  } else if (personAge >= 2 && personAge < 4) {
+//     console.log("you are a toddler") 
+//  }  else if (personAge >= 4 && personAge < 13) {
+//     console.log("you are a kid") 
+//  }  else if (personAge >= 13 && personAge < 20) {
+//     console.log("you are a teenager") 
+//  } else if (personAge >= 20 && personAge < 65) {
+//     console.log("you are a adult") 
+//  } else if (personAge >= 65 ) {
+//     console.log("you are a older") 
+//  }  
+//  //Question # 29:
+//  let favorite_fruite:string[]=["banana","orange","mango"]
+//  if (favorite_fruite[2] =="mango" ) {
+//     console.log("You rally like mango")
+//  }
+//  if (favorite_fruite[0] =="banana" ) {
+//     console.log("You rally like banana")
+//  }
+//  if (favorite_fruite[1] =="orange" ) {
+//     console.log("You rally like orange")
+//  }
+//  if (favorite_fruite[3] =="Apple" ) {
+//     console.log("You have not an apple")
+//  }
+//  if (favorite_fruite[4] =="graps" ) {
+//     console.log("You have not graps")
+//  }
+//Question # 30:
+//  let  userName:string[]=[
+//     "Heer","Kanwal","Abiha","Maheen","Admin"
+//  ]
+//  for (let index = 0; index < userName.length; index++) {
+//     const result = userName[index];
+//     if (result == "Admin") {
+//         console.log(`Hello Admin,whould you like to see a status report`)
+//     }else if(result == "Heer"){
+//         console.log("Hello Heer,thank you for log in again ")
+//     }else if(result == "Kanwal"){
+//         console.log("Hello Kanwal,thank you for log in again ")
+//     }else if(result == "Abiha"){
+//         console.log("Hello Abiha,thank you for log in again ")
+//     }else if(result == "Maheen"){
+//         console.log("Hello Maheen,thank you for log in again ")
+//     }
+//  }
+//Question # 31:
+//  let  userName1:string[]=[
+//     "Heer","Kanwal","Abiha","Maheen","Admin"]
+//    for (let index = 0; index < userName1.length; index++) {
+//     const element = userName1[index];
+//     if (element !=="") {
+//         console.log("list is not empty")
+//     }
+//    }
+//    let  userName2:string[]=[]
+//    for (let index1 = 0; index1<1; index1++) {
+//     const element1 = userName2[index1]
+//     if ( element1!=="") {
+//         console.log("list is not empty")
+//     }else{
+//         console.log("we need to find some users")
+//     }
+//    }
+// Question # 32:
+// let current_users:string[]=["ali","bilawal","sani","amina","hooriya"]
+// for (let index = 0; index < current_users.length; index++) {
+//     const currenUser = current_users[index];
+// let new_users:string[]=["ali","kamran","haniya","hooriya","mahnoor"]
+// for (let index = 0; index < new_users.length; index++) {
+//     const newUser = new_users[index];
+//     if (newUser == currenUser) {
+//         console.log("we need a new user")
+//     }else{
+//         console.log("you are our new user")
+//     }
+// }
+// }
+// Questyion # 33:
+// let ordinalNumber:string[]=["1st","2nd","3rd","4th","5th","6th","7th","8th","9th"]
+//     for (let index = 0; index < ordinalNumber.length; index++) {
+//         const ordinal_number = ordinalNumber[index];
+//         if (ordinal_number.slice(1,3) =="st" ) {
+//             console.log("1st")
+//         } else  if(ordinal_number.slice(1,3) == "nd"){
+//             console.log("2nd")
+//         }else  if(ordinal_number.slice(1,3) == "rd"){
+//             console.log("3rd")
+//     } else if (ordinal_number.slice(0,3) == "4th") {
+//          console.log("4th")
+//     }else if (ordinal_number.slice(0,3) == "5th") {
+//         console.log("5th")
+//    }else if (ordinal_number.slice(0,3) == "6th") {
+//     console.log("6th")
+// }else if (ordinal_number.slice(0,3) == "7th") {
+//     console.log("7th")
+// }else if (ordinal_number.slice(0,3) == "8th") {
+//     console.log("8th")
+// }else if (ordinal_number.slice(0,3) == "9th") {
+//     console.log("9th")
+// }
+// }
+//Question # 34:
+// let favoritePiza:string[]=["Neapolitan piza","Chicago piza","Sicilian piza"];
+// for (let index = 0; index < favoritePiza.length; index++) {
+//     const piza = favoritePiza[index];
+//     console.log(piza)
+// }
+// let favoritePizas:string[]=["Neapolitan piza","Chicago piza","Sicilian piza"];
+// for (let index = 0; index < favoritePiza.length; index++) {
+//     const pizas = favoritePiza[index];
+//     console.log(`I like ${pizas}`)
+// }
+// console.log(`I like ${favoritePizas[0]} it is vary testy to eat,\nand i also like ${favoritePizas[1]}becuase it also tasty and\nspicy and i love spicy food, and lastly ${favoritePizas[2]}\nis one of my favorite piza is is too spicy .\nI really love piza`)
+//Question 3 35:
+let animals = ["dog", "wolf", "loin"];
+for (let index = 0; index < animals.length; index++) {
+    const animal = animals[index];
+    console.log(animal);
+    console.log(`${animal} is a animal whoh have sharp teeth`);
+}
+console.log("Any of these animal are dangerous animals");
+export {};
+// //Question # 36:
+// function make_shirt(size:number = 0,message:string ="T-shirt") {
+//     console.log(`The size of T-shirt is ${size} and the message on the T-shirt is ${message}`)
+// }
+// make_shirt(23,'"The best T-shirt"');//small size
+// //Question # 37:
+// make_shirt(36,"I love typescript");//large size
+// make_shirt(50,"I love Python");//large size
+//Question # 38:
+// function discribe_city(city:string,country:string="Pakistan") {
+//     console.log(`${city} is in ${country}`)
+// }
+// discribe_city("Karachi");
+// discribe_city("Lahore");
+// discribe_city("dhaka");
+//Question # 39:
+// function city_country(city1:string,country1:string) {
+//     return console.log(`"${city1},${country1}"`)
+// }
+// city_country("Lahore","Pakistan")
+// city_country("Karachi","Pakistan")
+// city_country("islamabad","Pakistan")
+//QUESTION # 40:
+// function make_Album(name:string,title:string,tracks?:number) {
+//      return  console.log( {
+//         artistName:name,
+//         Title: title,
+//         tracks: tracks
+//     })
+// }
+// make_Album("Vermeer",'"Remarkably"')
+// make_Album("Leonardo da Vinci",'"The original Renaissance Man"')
+// make_Album("Rembrandt",'"One the greatest artists"')
+// make_Album("Michael Jackson",'"King and Queen of pop"',15);
+// //Question # 41:
+// let magicians:string[]=["Criss Angel",
+//     "David Copperfield",
+//    " David Blaine",
+//    " Penn ", "Teller",
+//     "Helder Guimarae"]
+//     function show_magicians() {
+//         for (let index = 0; index < magicians.length; index++) {
+//             const magician = magicians[index];
+//             console.log(magician)
+//         }
+//     }
+//     show_magicians();
+// Question # 42:
+// let greatMagicians:string[]=["Criss Angel","David Copperfield"," David Blaine"," Penn ", "Teller","Helder Guimarae"]
+// function show_magicians1(){
+//     for (let index = 0; index <  greatMagicians .length; index++) {
+//         const magician = greatMagicians[index];
+//         console.log(magician)
+//     }
+// }
+//  show_magicians1();
+// function make_great(great:string) {
+//     for (let index = 0; index < greatMagicians.length; index++) {
+//         const greatMegician = greatMagicians[index];
+//         console.log(`${great} ${greatMegician}`)
+//     }
+// }
+// make_great("The Great ")
+// //Question # 43:
+// show_magicians1()
+// make_great("The Grea ")
+//Question # 44:
+// function sandwitches(items:string[]) {
+//     return console.log(`I want these itmes in a sandwitch ${items}`)
+// }
+// sandwitches(["chicken", "chunks"," chilli"])
+// sandwitches([" onions", "bell peppers"," honey"])
+// sandwitches(["cheaz","katchup","kabab"])
+//Question # 45:
+// interface Car {
+//     manufacturer: string;
+//     model: string;
+//     [key: string]: any; 
+// }
+// function car_information(manufacturer: string, model: string, ...args: [string, any][]): Car {
+//     const car: Car = {
+//         manufacturer: manufacturer,
+//         model: model,
+//     }
+//     args.forEach(([key, value]) => {
+//         car[key] = value;
+//     });
+//     return car;
+// }
+// const car: Car = car_information('Toyota', 'Camry', ['color', 'blue'], ['optional_feature', 'sunroof']);
+// console.log(car);
+// interface Cartype {
+//     model : string,
+//     manufacturer: string,
+//     [key:string]:any
+// }
+// function myCar(model:string,manufacturer:string, ...args:[string,any][]) {
+//    const car1: Cartype ={
+//     model: model,
+//     manufacturer:manufacturer,
+//    }
+//     args.forEach(([key ,value]) =>{
+//         car1[key] = value;
+//     });
+//       return car1          
+//    }
+//    const car:Cartype = myCar("Tyota","Camry",["color","red"],["roof",["sunroof"]])
+// console.log(car)
